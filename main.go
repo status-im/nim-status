40884ddb449bca0fd22805441257b5101bddbf59
Oskar Thoren <ot@oskarthoren.com>
2019-06-29 10:01:15 +0800

    Increase logging

--- main.go
+++ main.go
@@ -19,10 +19,11 @@ func poll() {
 }
 
 func Start() {
-
 	C.NimMain()
+	fmt.Println("[nim-status] Start 1")
 	fmt.Println(C.nimbus_start(30306))
 	//C.nimbus_subscribe(C.CString("status-test-c"), nil)
+	fmt.Println("[nim-status] Start 2")
 
 	peer1 := "enode://2d3e27d7846564f9b964308038dfadd4076e4373ac938e020708ad8819fd4fd90e5eb8314140768f782db704cb313b60707b968f8b61108a6fecd705b041746d@192.168.0.33:30303"
 	peer2 := "enode://4ea35352702027984a13274f241a56a47854a7fd4b3ba674a596cff917d3c825506431cf149f9f2312a293bb7c2b1cca55db742027090916d01529fe0729643b@206.189.243.178:443"
@@ -36,15 +37,17 @@ func Start() {
 }
 
 func ListenAndPost() {
+	fmt.Println("[nim-status] ListenAndPost 1")
 	i := 0
 	for {
+		fmt.Println("[nim-status] ListenAndPost (post @i==1000) i= ", i)
 		C.nimbus_poll()
 		t := time.Now().UnixNano() / int64(time.Millisecond)
 		i = i + 1
 		time.Sleep(1 * time.Microsecond)
 		message := fmt.Sprintf("[\"~#c4\",[\"Message:%d\",\"text/plain\",\"~:public-group-user-message\",%d,%d,[\"^ \",\"~:chat-id\",\"status-test-c\",\"~:text\",\"Message:%d\"]]]", i, t*100, t, i)
 		if i%1000 == 0 {
-			fmt.Println("posting", message)
+			fmt.Println("[nim-status] posting", message)
 			C.nimbus_post(C.CString(message))
 		}
 	}
