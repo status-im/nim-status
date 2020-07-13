3cafe0ed43b9ff4c43beb1f69a726322d2da2f2e
Richard Ramos <info@richardramos.me>
2020-07-13 10:40:58 -0400

    fix: linking libstatus

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,2 +1,6 @@
+{.passL:"vendor/status-go/build/bin/libstatus.a"}
+
+import ../src/nim_status
+
 echo "Executing test 1"
-assert 1 == 1
+assert hashMessage("qwerty") == """{"result":"0xfeeeea836fc42840b42a5af9bdcdab1d0e3a9b5168bf46fa25c3704c810eadbe"}"""
