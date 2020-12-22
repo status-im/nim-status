ab3f83f274fcc3b51ab5cc97032178b46414ee6b
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2020-12-22 12:56:40 -0600

    feat: mailservers CRUD

---
+++ test/nim/mailservers.nim
@@ -0,0 +1,53 @@
+import sqlcipher
+import os, json, json_serialization
+import options
+import ../../nim_status/lib/mailservers
+import ../../nim_status/lib/database
+
+let passwd = "qwerty"
+let path = currentSourcePath.parentDir() & "/build/my.db"
+let db = initializeDB(path, passwd)
+
+let mailserver1 = Mailserver(
+  id: "mailserver-1",
+  name: "foo",
+  address: "bar",
+  password: some("baz"),
+  fleet: "quux"
+)
+
+let mailserver2 = Mailserver(
+  id: "mailserver-2",
+  name: "foo",
+  address: "bar",
+  password: some("baz"),
+  fleet: "quux"
+)
+
+let mailserver3 = Mailserver(
+  id: "mailserver-3",
+  name: "foo",
+  address: "bar",
+  password: some("baz"),
+  fleet: "quux"
+)
+
+db.saveMailserver(mailserver1)
+db.saveMailservers(@[mailserver2, mailserver3])
+
+var dbMailservers = db.getMailservers()
+
+echo dbMailservers
+
+assert dbMailservers.len == 3
+
+db.deleteMailserver(mailserver1)
+
+dbMailservers = db.getMailservers()
+
+echo dbMailservers
+
+assert dbMailservers.len == 2
+
+db.close()
+removeFile(path)
