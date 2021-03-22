1ebfa441275007429d285bd24dd1d4f355c1aee3
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2021-03-22 18:10:44 -0500

    WIP: changes to fix "sqlite out of memory error" and other small improvements
    The most important thing was to move the status-go compat sqlcipher pragmas to
    run immediately after the key'ing of the database in `initializeDB` in
    nim_status/database.nim and before the migrations are run.

--- nim_status/client.nim
+++ nim_status/client.nim
@@ -29,16 +29,12 @@ proc openAccounts*(self: StatusObject): seq[Account] =
 
 proc login*(self: StatusObject, keyUid: string, password: string) =
   self.userDB = initializeDB($self.config.rootDataDir / keyUid & ".db", password, app_migration.newMigrationDefinition(), true) # Disabling migrations because we are reusing a status-go DB
-  self.userDB.execScript("PRAGMA cipher_page_size = 1024");
-  self.userDB.execScript("PRAGMA kdf_iter = 3200");
-  self.userDB.execScript("PRAGMA cipher_hmac_algorithm = HMAC_SHA1");
-  self.userDB.execScript("PRAGMA cipher_kdf_algorithm = PBKDF2_HMAC_SHA1");
   echo "==============================="
   echo "DB path: " & ($self.config.rootDataDir / keyUid & ".db")
   echo "Password: " & password
   let result = self.userDB.value("SELECT public_key from settings") # check if decryption worked
   echo "Result: "
-  echo result.get()
+  echo $result
 
 proc closeUserDB*(self: StatusObject) =
   self.userDB.close()
